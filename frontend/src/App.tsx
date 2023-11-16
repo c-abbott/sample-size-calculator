9cc05593b7b1406a1160d56f1314475fad64d512
c-abbott <cllmbbtt@gmail.com>
2023-11-16 19:21:35 +0000

    Passing data to react component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import ListGroup from "./components/ListGroup";
+
 function App() {
+  let items = ["New York", "San Francisco", "Boston", "Los Angeles", "Tokyo"];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
