07b1f82df58e50884b08b79a52db4edc3368e8bc
c-abbott <cllmbbtt@gmail.com>
2023-11-16 19:17:20 +0000

    First react hook

--- frontend/src/components/ListGroup.tsx
+++ frontend/src/components/ListGroup.tsx
@@ -1,20 +1,26 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Boston", "Los Angeles", "Tokyo"];
-
-  // Event handler
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
