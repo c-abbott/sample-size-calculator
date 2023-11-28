fd35b74934b9349b67e53599feab5275ce31284e
c-abbott <cllmbbtt@gmail.com>
2023-11-28 19:17:57 +0000

    dont bother clipping

--- src/components/SampleSizeDisplay.tsx
+++ src/components/SampleSizeDisplay.tsx
@@ -14,10 +14,10 @@ const SampleSizeDisplay: React.FC<SampleSizeDisplayProps> = ({
 
   return (
     <div className="bg-dark-700 shadow-md rounded-md flex flex-col justify-center items-center p-24">
-      <strong className="text-lg font-bold text-accent mb-2 overflow-clip">
+      <strong className="text-lg text-center font-bold text-accent mb-2 overflow-clip">
         Sample Size
       </strong>
-      <div className="text-5xl font-extrabold text-primary overflow-clip">
+      <div className="text-5xl font-extrabold text-primary">
         {displaySize}
       </div>
     </div>
