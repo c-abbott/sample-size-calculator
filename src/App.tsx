6e89028eba5ea5ca1b2a75d2959ccf37dbe0f5ae
c-abbott <cllmbbtt@gmail.com>
2023-11-29 22:00:32 +0000

    Add binary calculator to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import BinarySampleSizeCalculator from "./components/BinarySampleSizeCalculator";
 import ContinuousSampleSizeCalculator from "./components/ContinuousSampleSizeCalculator";
 
 function App() {
   return (
     <div className="bg-dark-900 min-h-screen text-primary">
       <ContinuousSampleSizeCalculator />
+      <BinarySampleSizeCalculator />
     </div>
   );
 }
--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -15,6 +15,24 @@ function calculateZScore(p: number): number {
   return z;
 }
 
+export const calculateBinarySampleSize = (
+  mde: number, 
+  baselineConversion: number, 
+  alpha: number, 
+  power: number
+): number => {
+  const decimalMde = mde / 100; // Convert MDE from percentage to decimal
+  const decimalBaselineConversion = baselineConversion / 100; // Convert baseline conversion from percentage to decimal
+  const deltaSquared = decimalBaselineConversion * (1 - decimalBaselineConversion);
+
+  // Z-scores for Alpha and Power
+  const Za = calculateZScore(1 - alpha / 2); // For two-tailed test
+  const Zb = calculateZScore(power);
+
+  // Adjust sample size calculation
+  return 2 * Math.pow(Za + Zb, 2) * deltaSquared / Math.pow(decimalMde, 2);
+};
+
 export function calculateSampleSize(delta: number, avg: number, sd: number, alpha: number, beta: number): number {
   const Za = calculateZScore(1 - alpha / 2);
   const Zb = calculateZScore(1 - beta);
