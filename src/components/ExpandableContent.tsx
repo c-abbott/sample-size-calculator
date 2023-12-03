f7978f3dd051036b3e426606289d5383a4f18112
c-abbott <cllmbbtt@gmail.com>
2023-12-03 09:37:13 +0000

    Expandable content design tweaks

--- src/components/ExpandableContent.tsx
+++ src/components/ExpandableContent.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const ExpandableContent: React.FC<{ title: string, content: string }> = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false); // New state to track hover
 
   return (
-    <div className="bg-dark-800 rounded-md">
+    <div 
+      className="bg-dark-800 rounded-md"
+      onMouseEnter={() => setIsHovered(true)} // Set hover state
+      onMouseLeave={() => setIsHovered(false)} // Unset hover state
+    >
       <button
-        className="text-sm px-4 py-4 text-gray-500"
+        className="flex items-center text-sm px-4 py-4 text-gray-500 w-full text-left"
         onClick={() => setIsOpen(!isOpen)}
       >
+        <FontAwesomeIcon 
+          icon={isOpen ? faChevronDown : faChevronRight} 
+          className={`text-accent mr-4 ${isHovered ? 'opacity-100' : 'opacity-50'}`} // Conditional class for opacity
+        />
         {title}
       </button>
-      {isOpen && <p className="mt-2 px-4 py-4 text-sm text-gray-500">{content}</p>}
+      {isOpen && <p className="mt-2 ml-8 px-4 py-4 text-sm text-gray-500">{content}</p>}
     </div>
   );
 };
