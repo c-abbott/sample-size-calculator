88eb50b9eb6fb7405a795543afb876abbe3256b8
c-abbott <cllmbbtt@gmail.com>
2023-12-07 18:14:18 +0000

    Extend expandable content

--- src/components/ExpandableContent.tsx
+++ src/components/ExpandableContent.tsx
@@ -5,18 +5,18 @@ import {
   faChevronDown,
 } from "@fortawesome/free-solid-svg-icons";
 
-const ExpandableContent: React.FC<{ title: string; content: string }> = ({
-  title,
-  content,
-}) => {
+const ExpandableContent: React.FC<{
+  title: string;
+  children: React.ReactNode;
+}> = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isHovered, setIsHovered] = useState(false); // New state to track hover
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div
       className="bg-dark-800 rounded-md"
-      onMouseEnter={() => setIsHovered(true)} // Set hover state
-      onMouseLeave={() => setIsHovered(false)} // Unset hover state
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <button
         className="flex items-center text-sm px-4 py-4 text-gray-500 w-full text-left"
@@ -26,12 +26,14 @@ const ExpandableContent: React.FC<{ title: string; content: string }> = ({
           icon={isOpen ? faChevronDown : faChevronRight}
           className={`text-accent mr-4 ${
             isHovered ? "opacity-100" : "opacity-50"
-          }`} // Conditional class for opacity
+          }`}
         />
         {title}
       </button>
       {isOpen && (
-        <p className="mt-2 ml-8 px-4 py-4 text-sm text-gray-400">{content}</p>
+        <div className="mt-2 ml-8 px-4 py-4 text-sm text-gray-400">
+          {children}
+        </div>
       )}
     </div>
   );
